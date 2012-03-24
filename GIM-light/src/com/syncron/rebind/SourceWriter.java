c9b823e559333843146bdbee1aad955b3d863fe7
Maciej Jaśkowski <maciej.jaskowski@gmail.com>
2012-03-24 21:05:03 +0100

    refactoring

---
+++ GIM-light/src/com/syncron/rebind/SourceWriter.java
@@ -0,0 +1,79 @@
+package com.syncron.rebind;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Iterator;
+
+public class SourceWriter {
+
+	private String str = "";
+	private String indent = "";
+	private boolean newLine = true;
+
+	public SourceWriter(OutputStreamWriter outputStreamWriter) {
+	}
+
+	public void openBlock(String string) {
+		println(string + " {");
+		indent();
+	}
+
+	public void doReturn(String string) {
+		println("return " + string + ";");
+	}
+
+	public void closeBlock() {
+		outdent();
+		println("}");		
+	}
+
+	public void print(String string) {
+		if (newLine) {
+			str += indent;
+		}
+		str += string;
+		newLine = false;
+	}
+
+	public void indent() {
+		indent  += "  ";
+	}
+
+	public void println(String string) {
+		try {
+			print(string);
+			newLine();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	void printList(Iterable<?> iterable) {
+		printList(iterable.iterator());
+	}
+	
+	void printList(Iterator<?> iterator) {
+		while(iterator.hasNext()) {
+			Object method = iterator.next();
+			print("\"" + method.toString() + "\"");
+			if (iterator.hasNext()) {
+				print(", ");
+			}
+		}
+	}
+
+	public void newLine() throws IOException {
+		str += "\n";
+		newLine = true;
+	}
+
+	public void outdent() {
+		indent = indent.substring(0, indent.length() - 2);		
+	}
+	
+	@Override
+	public String toString() {
+		return str;
+	}
+	
+}
